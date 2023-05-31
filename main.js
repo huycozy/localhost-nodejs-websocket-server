c3343cb16d3fef50595c99fe6d0cad3f723b7810
huycozy <huy@nevercode.io>
2023-05-31 09:05:57 +0700

    update

--- main.js
+++ main.js
@@ -6,14 +6,17 @@ const wss = new WebSocketServer.Server({ port: 8080 })
  
 // Creating connection using websocket
 wss.on("connection", ws => {
-    console.log("new client connected");
+    console.log("A new client connected");
     // sending message
     ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`)
+        console.log(`Client has sent us: ${data}`);
+        // send back
+        console.log("Throwing back: " + data.toString());
+        ws.send(data.toString());
     });
     // handling what to do when clients disconnects from server
     ws.on("close", () => {
-        console.log("the client has connected");
+        console.log("Client has disconnected");
     });
     // handling client connection error
     ws.onerror = function () {
